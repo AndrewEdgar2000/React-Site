41921bc0bf3fb457ed413f1d00189595abc717b8
Andrew Edgar <edgar.andrew@mail.com>
2022-11-17 15:15:14 -0800

    Bug with heroSection. Video will not show up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact/>
+        <Route path='/' exact componant={Home}/>
       </Routes>
     </Router>
     </>
