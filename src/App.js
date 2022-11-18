1ff0f061ccbfce8eafe1cd153c33b07061748b42
Andrew Edgar <edgar.andrew@mail.com>
2022-11-17 21:37:53 -0800

    Added other sections of website, Still not rendering

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path='/' component={Home}/>
+        <Route path='/' component={Home}/> 
       </Routes>
     </Router>
     </>
