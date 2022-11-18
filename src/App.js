6af399344acfd4eb53859b300d10b96b89c19200
Andrew Edgar <edgar.andrew@mail.com>
2022-11-18 12:28:09 -0800

    Finished front end markup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   console.log(window.location);
@@ -12,6 +15,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/> 
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
       </Routes>
     </Router>
     </>
